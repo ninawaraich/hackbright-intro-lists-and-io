4ab7520423577905613dfef1b38d9f7f5df2c745
Sara and Nina <nina_singh@hotmail.com>
2015-11-16 20:18:41 -0800

    more more changes!

--- file-exercises/compare_favs.py
+++ file-exercises/compare_favs.py
@@ -1,10 +1,20 @@
-nina_fav_foods = open('nina_fav_foods.txt')
-nina_fav_foods.readlines()
-nina_fav_foods.close()
+def opening_files(name):
+	fav_foods = open(name)
+	fav_foods_list = fav_foods.readlines()
+	fav_foods.close()
+	return fav_foods_list
 
-sara_fav_foods = open('sara_fav_foods.txt')
-sara_fav_foods.readlines()
-sara_fav_foods.close()
+
+nina_fav_foods = opening_files("nina_fav_foods.txt")
+sara_fav_foods = opening_files("sara_fav_foods.txt")
+
+print nina_fav_foods, sara_fav_foods
+
+if nina_fav_foods[0]==sara_fav_foods[0]:
+	print "Our favorite foods are the same :)"
+else:
+	print "Our favorite foods are different :("
+
 
 
 
