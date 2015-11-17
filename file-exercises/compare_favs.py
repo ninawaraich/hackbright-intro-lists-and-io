5535f724506e92a6902d1b78be5d6647cc2d6efc
Sara and Nina <nina_singh@hotmail.com>
2015-11-16 20:30:15 -0800

    finallllll

--- file-exercises/compare_favs.py
+++ file-exercises/compare_favs.py
@@ -10,10 +10,15 @@ sara_fav_foods = opening_files("sara_fav_foods.txt")
 
 print nina_fav_foods, sara_fav_foods
 
-if nina_fav_foods[0]==sara_fav_foods[0]:
-	print "Our favorite foods are the same :)"
-else:
-	print "Our favorite foods are different :("
+#if nina_fav_foods[0]==sara_fav_foods[0]:
+	#print "Our favorite foods are the same :)"
+#else:
+	#print "Our favorite foods are different :("
+
+for x in [0,1,2]:
+	if nina_fav_foods[x] in sara_fav_foods:
+		food=nina_fav_foods[x]
+		print "We both love", food
 
 
 
